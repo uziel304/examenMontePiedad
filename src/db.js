b7d784747196f7b805df7d1f0cb2d5e14a5b31d4
uziel304 <uziel304@gmail.com>
2023-12-11 21:54:43 -0700

    se cambio la ruta de bd

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () =>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/montePiedad');
+        await mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1');
         console.log('Is Connected DB');
     } catch (error) {
         console.log(error);
