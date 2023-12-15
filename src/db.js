0f4e204c0b634d1971a935fd10b6c03a19b4f0d7
uziel304 <uziel304@gmail.com>
2023-12-14 17:51:18 -0700

    db modify

--- src/db.js
+++ src/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () =>{
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1');
+        
         console.log('Is Connected DB');
     } catch (error) {
         console.log(error);
